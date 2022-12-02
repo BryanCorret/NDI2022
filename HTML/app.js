0e829a9903cb5bb851c89d5fde77a7c927db6d06
Farault <95053540+theofarault@users.noreply.github.com>
2022-12-02 04:44:36 +0100

    Delete app.js

--- HTML/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $.getJSON("DATA/detailsData.json", function(data){
-        console.log(data); // Prints: Harry
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
-});
