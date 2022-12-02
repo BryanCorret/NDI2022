d9690175eeb8bc9317e8710d26c3e51d5aa4880f
Bryan <98695162+BryanCorret@users.noreply.github.com>
2022-12-02 05:11:31 +0100

    CSS +JS

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+// darkmode
+
+const themeToggler = document.querySelector('.theme-toggler');
+ 
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    });
