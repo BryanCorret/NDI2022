7a69fd1db700ffa858970a258778094c3b966ad5
KISSOUNA Bradley <o22101575@info25-09.iut45.univ-orleans.fr>
2022-12-02 05:57:19 +0100

    ajout fonctionalité

--- HTML/script.js
+++ HTML/script.js
@@ -5,16 +5,57 @@ counters.forEach( counter => {
    const animate = () => {
       const value = +counter.getAttribute('value');
       const data = +counter.innerText;
-     
+
       const time = value / speed;
+      
      if(data < value) {
           counter.innerText = Math.ceil(data + time);
           setTimeout(animate, 1);
-        }else{
-          counter.innerText = value;
         }
-     
+        else{
+          const str_chiffre = value.toString();
+          const taille = str_chiffre.length;
+          let div = taille%3
+          let cpt=0;
+          let ee = " ";
+          
+          
+          for(var i=0;i< str_chiffre.length;i++){
+            if (div ==0){
+              cpt+=3
+              div=2
+              ee = ee.concat(str_chiffre[i]);
+            }
+
+            else if(i==1){
+
+              if(i == div){
+                ee = ee.concat("."+str_chiffre[i]);
+              }
+              else{
+                ee = ee.concat((str_chiffre[i]));
+              }
+            }
+            else if(i == div){
+              ee = ee.concat(str_chiffre[i]+".");
+            }
+
+            else if(cpt%3 == 0){
+              ee = ee.concat((str_chiffre[i]));
+            }
+
+            else{
+              ee = ee.concat(str_chiffre[i]);
+            }
+            
+          }
+          counter.innerText = ee;
+        }
+
    }
-   
+
    animate();
+
+  
+
 }); /*Call this funtion with the ID-name for that element to increase the number within*/};
